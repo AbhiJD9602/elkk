d0316b3222ae669fa1bad5cfbc80a3fcd5cf0ea7
AbhiJD9602 <abhinav.soni13@gmail.com>
2020-06-07 23:10:59 +0530

    updated script and added documentation

--- start-apps.sh
+++ start-apps.sh
@@ -6,4 +6,4 @@ echo "Starting application..."
 docker run -d --rm --name application \
   -p 9082:9082 --network elkk_default -e ZIPKIN_HOST=zipkin \
       --health-cmd="curl -f http://localhost:9082/actuator/health || exit 1" --health-start-period=1m \
-  application
+  elkk/application
